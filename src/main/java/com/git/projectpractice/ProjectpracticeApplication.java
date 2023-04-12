452751be0e9b04cac7f0d9c2b93db062fdbdb412
이인건 <dlsrjsdl@naver.com>
2023-04-12 10:43:09 +0900

    초기버전

---
+++ src/main/java/com/git/projectpractice/ProjectpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.git.projectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/projectpractice/ProjectpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.projectpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
